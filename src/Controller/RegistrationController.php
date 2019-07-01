bfa5617fc8eb08f9a6a8c36f889e55a6bd7ccfd5
iulian_1976 <iulian_bulan@yahoo.com>
2019-07-01 10:29:47 +0200

    commit-creation Registration&Connex&Contact

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -15,8 +15,9 @@ class RegistrationController extends AbstractController
     /**
      * @Route("/")
      */
-    public function index()
+    public function register()
     {
+
         return $this->render('registration/index.html.twig', [
             'controller_name' => 'RegistrationController',
         ]);
