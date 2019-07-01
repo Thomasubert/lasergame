b2315134846094a96720a83a272a1e12c899cbcf
Ajoachim <alexiaj27@gmail.com>
2019-07-01 15:54:12 +0200

    add remember me function and icon

--- src/Controller/ConnexionController.php
+++ src/Controller/ConnexionController.php
@@ -4,22 +4,55 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 /**
  * Class ConnexionController
  * @package App\Controller
- * @Route("connexion ")
+ * @Route("/")
  */
 
 class ConnexionController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("login")
+     * @param AuthenticationUtils $utils
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function index(AuthenticationUtils $utils)
     {
+        $error = $utils->getLastAuthenticationError();
+        $lastUsername = $utils->getLastUsername();
+
+        if (!empty($error)){
+            $this->addFlash('error', 'Identifiants incorrects');
+        }
+
+
+
+
         return $this->render('connexion/index.html.twig', [
-            'controller_name' => 'ConnexionController',
+            'last_username' => $lastUsername
         ]);
+    }
+    
+    /**
+     * @Route("/déconnexion")
+     */
+
+    public function logout()
+    {
+
+    }
+
+
+    public function accountInfo()
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
+    }
+
+    public function resetPassword()
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
     }
 }
