4ab18840b9aee8417e69602b90daf3144e19717d
Ajoachim <alexiaj27@gmail.com>
2019-07-03 13:42:00 +0200

    add type in UserType

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -22,7 +22,7 @@ class RegistrationController extends AbstractController
      */
     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, EntityManagerInterface $em)
     {
-      
+
 
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
