4c8bba2d10a66355b8f1b3c031a6e2304e08998d
Ajoachim <alexiaj27@gmail.com>
2019-07-03 12:08:59 +0200

    delete if get user

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -22,9 +22,7 @@ class RegistrationController extends AbstractController
      */
     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, EntityManagerInterface $em)
     {
-      /*  if ($this->getUser()){
-            return $this->redirectToRoute('app_index_index');
-        }*/
+      
 
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
