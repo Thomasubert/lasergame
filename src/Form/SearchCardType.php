b6e6178715fdd4ec843e9405ce40a820c12de755
TomWF3 <t.hubert@live.fr>
2019-07-08 16:20:44 +0200

    card search finished

--- src/Form/SearchCardType.php
+++ src/Form/SearchCardType.php
@@ -5,16 +5,20 @@ namespace App\Form;
 
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 
+/**
+ * Class SearchCardType
+ * @package App\Form
+ */
 class SearchCardType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('codeCentre')
-            ->add('codeCarte')
-            ->add('checkSum')
+            ->add('codeCard')
+            ->add('rechercher', SubmitType::class)
         ;
     }
 }
