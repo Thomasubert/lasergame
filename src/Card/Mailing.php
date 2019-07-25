938031db2685baa416418ae58d85a3612293d61c
ajoachim <alexia-joachim@hotmail.fr>
2019-07-25 08:22:55 +0200

    Update Mailing.php

--- src/Card/Mailing.php
+++ src/Card/Mailing.php
@@ -6,5 +6,32 @@ namespace App\Card;
 
 class Mailing
 {
+    public function index($name, \Swift_Mailer $mailer)
+    {
+        $message = (new \Swift_Message('Hello Email'))
+            ->setFrom('send@example.com')
+            ->setTo('recipient@example.com')
+            ->setBody(
+                $this->renderView(
+                // templates/emails/registration.html.twig
+                    'emails/registration.html.twig',
+                    ['name' => $name]
+                ),
+                'text/html'
+            )
 
+            // you can remove the following code if you don't define a text version for your emails
+            ->addPart(
+                $this->renderView(
+                    'emails/registration.txt.twig',
+                    ['name' => $name]
+                ),
+                'text/plain'
+            )
+        ;
+
+        $mailer->send($message);
+
+        return $this->render(...);
+    }
 }
